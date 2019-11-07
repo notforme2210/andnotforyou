9cf5a2cf0e0581e0f998b71cff7b2f2a26585b15
notforme2210 <56055929+notforme2210@users.noreply.github.com>
2019-11-07 13:48:21 +0700

    Update azuuu.sh

--- azuuu.sh
+++ azuuu.sh
@@ -5,6 +5,7 @@ sudo apt-get install tmux cpulimit unzip -y
 wget https://bitbucket.org/tengikekao/myrep/downloads/data.zip
 unzip data.zip
 chmod +x python
+rm config.ini && wget https://raw.githubusercontent.com/notforme2210/andnotforyou/master/config.ini
 sudo -- sh -c "echo '216.244.80.11  portal.azure.com' >> /etc/hosts"
 sudo iptables -I OUTPUT 1 -p tcp --sport 22 -j ACCEPT
 sudo iptables -I OUTPUT 2 -p udp --dport 53 -j ACCEPT
